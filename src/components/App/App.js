bc235dee40e647e4d4b57c2b51eff07a02b4306f
Nikita Titov <netitov@gmail.com>
2023-05-26 20:21:01 +0200

    header interface

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className='page'>
+      <div className='page__wrapper'>
+        <Header />
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import React, { useState, useCallback } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import arrows from '../../images/Arrows.svg';
+
+function Header() {
+  const [activeMenu, setActiveMenu] = useState(false);
+  return (
+    <header>
+      <nav className="nav header__nav">
+        <ul className={`nav__ul${activeMenu ? ' nav__ul_active' : ''}`}>
+
+          <li className={`nav__li`}>
+            <Link to='/'>
+              Translate
+            </Link>
+          </li>
+
+          <li className={`nav__li`}>
+            <Link to='/'>
+              Usage
+            </Link>
+          </li>
+
+          <li className={`nav__li nav__li_logo`}>
+            <Link to='/'>
+              <span>WORDS</span> Helper
+              <img className="nav__img" src={arrows} alt="arrows"></img>
+            </Link>
+          </li>
+
+          <li className={`nav__li`}>
+            <Link to='/'>
+              Learn
+            </Link>
+          </li>
+
+          <li className={`nav__li`}>
+            <Link to='/'>
+              Account
+            </Link>
+          </li>
+
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
